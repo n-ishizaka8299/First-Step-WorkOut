28eddaa841ee55537419314949629e40ddf4cc7b
n-ishizaka8299 <n.ishizaka8299@gmail.com>
2021-05-09 15:57:38 +0900

    change STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -127,11 +127,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # staticフォルダの場所
 # STATICFILES_DIRS = [BASE_DIR / 'static/']
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # whitenoise設定
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'config.storage.WhiteNoiseStaticFilesStorage'
